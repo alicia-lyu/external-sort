c25a97140c46a3e06e8559e6ff5eb56affa17727
alicia-lyu <wlyu27@wisc.edu>
2024-04-06 21:46:44 -0500

    Fix memory calculation in MemoryRun

--- Data.cpp
+++ Data.cpp
@@ -4,7 +4,7 @@ MemoryRun::MemoryRun (u_int16_t count, RowSize size):
     count (count), size (size)
 {
     TRACE (true);
-    _rows = (byte *) malloc(sizeof(RowSize) * count);
+    _rows = (byte *) malloc(size * count * sizeof(byte));
 }
 
 MemoryRun::~MemoryRun ()
@@ -15,7 +15,7 @@ MemoryRun::~MemoryRun ()
 
 byte * MemoryRun::getRow (u_int16_t index)
 {
-    return _rows + index;
+    return _rows + index * size;
 }
 
 byte * MemoryRun::fillRowRandomly (u_int16_t index)

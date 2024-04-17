40df84611861b36c4b0d7f6687246d2d0343d40d
Yuheng <799810767@qq.com>
2024-04-16 22:47:08 -0500

    Generate random alphanumeric bytes

--- Buffer.cpp
+++ Buffer.cpp
@@ -1,19 +1,44 @@
 #include "Buffer.h"
 
 Buffer::Buffer (u_int16_t count, RowSize size):
-    count (count), size (size)
+    count (count), size (size), _device(), _engine(_device()), _distribution(0, RANDOM_BYTE_UPPER_BOUND - 1)
 {
     TRACE (false);
     _rows = (byte *) malloc(size * count * sizeof(byte));
     _read = _rows;
     _filled = _rows;
-    _engine.seed(_device());
 }
 
 Buffer::~Buffer ()
 {
     TRACE (false);
     free(_rows);
+}
+
+/*
+Generate random bytes and convert them to alphanumeric characters (A-Z, a-z, 0-9)
+randomByte: a random byte (0-61)
+0-9 -> '0'-'9' (48-57)
+10-35 -> 'A'-'Z' (65-90)
+36-61 -> 'a'-'z' (97-122)
+*/
+byte Buffer::toAlphaNumeric (const byte randomByte) {
+    const byte numericOffset = 48;
+    const byte upperCaseOffset = 65;
+    const byte lowerCaseOffset = 97;
+    if (randomByte < 10) {
+        return randomByte + numericOffset;
+    } else if (randomByte < 36) {
+        return randomByte - 10 + upperCaseOffset;
+    } else {
+        return randomByte - 36 + lowerCaseOffset;
+    }
+}
+
+byte Buffer::getRandomAlphaNumeric ()
+{
+    byte randomByte = _distribution(_engine);
+    return toAlphaNumeric(randomByte);
 }
 
 byte * Buffer::fillRandomly ()
@@ -23,7 +48,7 @@ byte * Buffer::fillRandomly ()
         // TODO: uniformize the behavior of overwriting the first row (return nullptr to indicate)
         _filled = _rows;
     }
-    std::generate(_filled, _filled + size, std::ref(_engine));
+    std::generate(_filled, _filled + size, [this](){ return getRandomAlphaNumeric(); });
     _filled += size;
     return _filled - size;
 }
